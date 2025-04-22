3a0968d801b11739d03a08e05c8c9f176fa7ac7e
echo2212 <echo221299@gmail.com>
2025-04-23 06:56:34 +0900

    Refactor imports

--- src/main/java/com/xpenses/transaction/controller/IncomeController.java
+++ src/main/java/com/xpenses/transaction/controller/IncomeController.java
@@ -10,8 +10,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IncomeController {
--- src/main/java/com/xpenses/transaction/service/impl/IncomeAcquirerService.java
+++ src/main/java/com/xpenses/transaction/service/impl/IncomeAcquirerService.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @Qualifier("incomeAcquirer")
 public class IncomeAcquirerService implements AcquirerService {
