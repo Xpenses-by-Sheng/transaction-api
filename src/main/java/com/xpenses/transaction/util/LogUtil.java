be47b854a6181bafec3e77b00edc996a52daedf5
echo2212 <echo221299@gmail.com>
2025-04-23 22:13:00 +0900

    Fixed LogUtil logger class name

--- src/main/java/com/xpenses/transaction/util/LogUtil.java
+++ src/main/java/com/xpenses/transaction/util/LogUtil.java
@@ -1,6 +1,5 @@
 package com.xpenses.transaction.util;
 
-import com.xpenses.transaction.controller.IncomeController;
 import com.xpenses.transaction.dto.request.ApiRequest;
 import com.xpenses.transaction.dto.response.ApiResponse;
 import org.apache.logging.log4j.LogManager;
@@ -8,13 +7,13 @@ import org.apache.logging.log4j.Logger;
 
 public class LogUtil {
 
-    private static final Logger logger = LogManager.getLogger(IncomeController.class);
+    private static final Logger logger = LogManager.getLogger(LogUtil.class);
 
-    public static void outputRequestLog(ApiRequest request){
-        logger.debug("Request: {}",request.toString());
+    public static void outputRequestLog(ApiRequest request) {
+        logger.debug("Request: {}", request.toString());
     }
 
     public static void outputResponseLog(ApiResponse response) {
-        logger.debug("Response: {}",response.toString());
+        logger.debug("Response: {}", response.toString());
     }
 }
